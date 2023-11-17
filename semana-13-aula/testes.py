1e682dcb6dce645e47ce440375457b802331f5ec
Jotta-Art-IFSP <112517025+Jotta-Art-IFSP@users.noreply.github.com>
2023-11-16 21:44:05 -0300

    Update testes.py

--- semana-13-aula/testes.py
+++ semana-13-aula/testes.py
@@ -1,34 +1,34 @@
 import unittest
-import aluno as a;
-import turma as t;
+from aluno import Aluno
+from turma import Turma
 
-class turmaTest(unittest.TestCase):
+class TurmaTest(unittest.TestCase):
 
-  def setUp(self):
-    print('Teste', self._testMethodName, 'sendo executado...');
-    self.alunos = [];
-    self.alunos.append(a.Aluno('Fabio', 'Teixeira', 11));
-    self.alunos.append(a.Aluno('Fabiano', 'Teixeira', 7));
-    self.alunos.append(a.Aluno('Melissa', 'Teixeira', 8));
-    self.alunos.append(a.Aluno('Rafael', 'Teixeira', 9));
-    self.alunos.append(a.Aluno('Angela', 'Teixeira', 6));
-    self.alunos.append(a.Aluno('Angela', 'Teixeira', -1));    
-    self.turmaObject = t.Turma();
-    self.turmaObject.cadastrarAlunos(self.alunos);
-  
-  def tearDown(self):
-    print('Teste', self._testMethodName, 'finalizado.'); 
-  
-  def testMaior(self):      
-    self.assertEqual(9, self.turmaObject.maiorNota.nota, 'Erro ao encontrar maior nota');
+    def setUp(self):
+        print('Teste', self._testMethodName, 'sendo executado...')
+        self.alunos = []
+        self.alunos.append(Aluno('Fabio', 'Teixeira', 11))
+        self.alunos.append(Aluno('Fabiano', 'Teixeira', 7))
+        self.alunos.append(Aluno('Melissa', 'Teixeira', 8))
+        self.alunos.append(Aluno('Rafael', 'Teixeira', 9))
+        self.alunos.append(Aluno('Angela', 'Teixeira', 6))
+        self.alunos.append(Aluno('Angela', 'Teixeira', -1))
+        self.turmaObject = Turma()
+        self.turmaObject.cadastrarAlunos(self.alunos)
 
-  def testMenor(self):    
-    self.assertEqual(6, self.turmaObject.menorNota.nota, 'Erro ao encontrar menor nota');
+    def tearDown(self):
+        print('Teste', self._testMethodName, 'finalizado.')
 
-  def testIntervalo(self):    
-    self.assertTrue((self.turmaObject.menorNota.nota > 0 and self.turmaObject.maiorNota.nota <= 10), 'Erro ao verificar intervalo')    
+    def testMaior(self):
+        self.assertEqual(9, self.turmaObject.maiorNota.nota, 'Erro ao encontrar maior nota')
+
+    def testMenor(self):
+        self.assertEqual(6, self.turmaObject.menorNota.nota, 'Erro ao encontrar menor nota')
+
+    def testIntervalo(self):
+        self.assertTrue((self.turmaObject.menorNota.nota > 0 and self.turmaObject.maiorNota.nota <= 10),
+                        'Erro ao verificar intervalo')
 
 
 if __name__ == "__main__":
-  unittest.main()
-
+    unittest.main()
