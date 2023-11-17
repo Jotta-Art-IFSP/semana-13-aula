d5a7c095fbe073883d01fda2b9112263d8494461
Jotta-Art-IFSP <112517025+Jotta-Art-IFSP@users.noreply.github.com>
2023-11-16 21:45:41 -0300

    Update main.py

--- semana-13-aula/main.py
+++ semana-13-aula/main.py
@@ -1,18 +1,18 @@
-import aluno as a;
-import turma as t;
+from aluno import Aluno
+from turma import Turma
 
 alunos = []
-alunos.append(a.Aluno('Fabio', 'Teixeira', 11));
-alunos.append(a.Aluno('Fabiano', 'Teixeira', 7));
-alunos.append(a.Aluno('Melissa', 'Teixeira', 8));
-alunos.append(a.Aluno('Rafael', 'Teixeira', 9));
-alunos.append(a.Aluno('Angela', 'Teixeira', 6));
+alunos.append(Aluno('Joao', 'Pedro', 11))
+alunos.append(Aluno('Maria', 'Silva', 7))
+alunos.append(Aluno('Carlos', 'Santos', 8))
+alunos.append(Aluno('Luisa', 'Fernandes', 9))
+alunos.append(Aluno('Antonio', 'Oliveira', 6))
 
-turmaObject = t.Turma();
-turmaObject.cadastrarAlunos(alunos);
+turmaObject = Turma()
+turmaObject.cadastrarAlunos(alunos)
 
-turmaObject.mostrarAlunos();
-print('*'*30)
+turmaObject.mostrarAlunos()
+print('*' * 30)
 print('Aluno com menor nota:', turmaObject.menorNota.mostrarAluno())
 print('Aluno com maior nota:', turmaObject.maiorNota.mostrarAluno())
-turmaObject.maiorNota.mostrarAluno();
+turmaObject.maiorNota.mostrarAluno()
