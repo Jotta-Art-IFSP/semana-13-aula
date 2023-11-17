d01472d1f2ceebe24dda391456c5c89e3d676736
Jotta-Art-IFSP <112517025+Jotta-Art-IFSP@users.noreply.github.com>
2023-11-16 21:43:04 -0300

    Update aluno.py

--- semana-13-aula/aluno.py
+++ semana-13-aula/aluno.py
@@ -1,8 +1,8 @@
 class Aluno:
-  def __init__(self, nome, sobrenome, nota):
-    self.nome = nome;
-    self.sobrenome = sobrenome;
-    self.nota = nota;
+    def __init__(self, nome, sobrenome, nota):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.nota = nota
 
-  def mostrarAluno(self):
-    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota);
+    def mostrarAluno(self):
+        return f'Aluno: {self.nome} {self.sobrenome} - Nota: {self.nota}'
